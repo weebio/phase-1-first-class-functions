d9f53c57dba0901a26cf32e83d7dfaf0b18c7e95
Evaristo <bubbali2002@gmail.com>
2021-07-22 11:04:08 -0500

    commit tree 5fd7a1492287c7d8664cc86afa15beb03a4a16dd

---
+++ index.js
@@ -0,0 +1,20 @@
+// function day() {
+//   console.log("monday");
+//   const weekday = day();
+//   console.log(weekday);
+// }
+// function call(call) {
+//   call()
+// }
+// function Sayhi() {
+//   console.log("hi");
+// }
+// call(Sayhi);
+
+// call(function Saybye() {
+//   console.log("bye")
+// })
+
+const funk = function(){
+  
+}
